8a81df9c5a2c8f31336df81d9eefea50780c265f
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-09-30 07:59:50 -0700

    a few comments after review

--- tests/NUnit.Tests.UI/Tests/TextBoxTests.cs
+++ tests/NUnit.Tests.UI/Tests/TextBoxTests.cs
@@ -4,30 +4,9 @@ namespace NUnit.Tests.UI.Tests;
 
 public class Tests
 {
-    [Test]
-    public void Test1()
-    {
-        var mainPaige = new MainPaige().OpenInFireFox();
-        var title = mainPaige.GetPageTitle;
-        var elementsPage = mainPaige.OpenElementsPage();
-        var isAccordionElementPresent = elementsPage.CheckAccordion();
+    //My example of test can be dropped
 
-        var textBoxPage = elementsPage.OpenTextBoxPage();
-        var textBoxForIsPresent =  textBoxPage.CheckTextBoxForm();
-        var textBoxTitle = textBoxPage.CheckTextBoxTitle();
-        var textBoxLabelName = textBoxPage.GetFullNameLabelText();
-            textBoxPage.EnterFullName("Oleh Kutafin");
-
-        Assert.Multiple(() =>
-        {
-            Assert.That(title, Is.EqualTo("DEMOQA"));
-            Assert.That(isAccordionElementPresent, Is.True);
-            Assert.That(textBoxForIsPresent, Is.True);
-            Assert.That(textBoxTitle, Is.True);
-            Assert.That(textBoxLabelName, Is.EqualTo("Full Name"));
-        });
-    }
-
+    //Should use [SetUp] and [TearDown] methods to make the test more readable
     [Test]
     public void TextBoxFieldsEntered()
     {
