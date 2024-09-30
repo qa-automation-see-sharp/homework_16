8a77101c703c044c2c260b92181a11d91b5bd4bf
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-09-30 07:49:19 -0700

    review fixes

--- tests/NUnit.Tests.UI/Tests/TextBoxTests.cs
+++ tests/NUnit.Tests.UI/Tests/TextBoxTests.cs
@@ -3,7 +3,7 @@ using Test.Utils.PageObjects;
 
 namespace NUnit.Tests.UI.Tests;
 
-[TestFixture, Parallelizable(ParallelScope.All)]
+[TestFixture] // [Parallelizable(ParallelScope.All)] won't work with the current implementation 
 public class TextBoxTests
 {
     protected MainPage _mainPage;
@@ -44,21 +44,23 @@ public class TextBoxTests
     }
 
     [Test]
-    public void NavigateToTextBox_WhenAllFieldsAreSubmitted_ShouldRetuntBriefDescription()
+    public void NavigateToTextBox_WhenAllFieldsAreSubmitted_ShouldReturnBriefDescription()
     {
         var enterFullName = _textBoxPage.EnterFullName("Zaremba Olesia");
         var enterEmail = _textBoxPage.EnterEmail("zaremba.olesia@gmail.com");
-        var enterCurrentAdress = _textBoxPage.EnterCurrentAddress("Khreshchatyk street, 45, Kyiv, Ukraine");
-        var enterPermanentAdress = _textBoxPage.EnterPermanentAddress("Khreshchatyk street, 45, Kyiv, Ukraine");
+        var enterCurrentAddress = _textBoxPage.EnterCurrentAddress("Khreshchatyk street, 45, Kyiv, Ukraine");
+        var enterPermanentAddress = _textBoxPage.EnterPermanentAddress("Khreshchatyk street, 45, Kyiv, Ukraine");
 
         _textBoxPage.Submit();
 
         var output = _textBoxPage.GetOutputText();
 
-        var textCheck = "Name:Zaremba Olesia\r\nEmail:zaremba.olesia@gmail.com\r\nCurrent Address :Khreshchatyk street, 45, Kyiv, Ukraine\r\nPermananet Address :Khreshchatyk street, 45, Kyiv, Ukraine";
+        var textCheck = "Name:Zaremba Olesia\nEmail:zaremba.olesia@gmail.com\nCurrent Address :Khreshchatyk street, 45, Kyiv, Ukraine\nPermananet Address :Khreshchatyk street, 45, Kyiv, Ukraine";
         Assert.Multiple(() =>
         {
             Assert.AreEqual(textCheck, output);
+            //Use That this is recommended by NUnit
+            Assert.That(output, Is.EqualTo(textCheck));
         });
     }
 
